688144db588df1ea753550fdac243d7a80ab3304
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-13 12:32:47 -0300

    Obtaining user by id

---
+++ src/main/java/com/motosimagab/springboot_mongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.motosimagab.springboot_mongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+}
