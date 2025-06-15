d1c5d205292457159e7eaa7dac735fee8a3940a5
Gabriel Motosima <gabriel.motosima@hotmail.com>
2025-06-15 17:33:24 -0300

    Simple consult with query methods

--- src/main/java/com/motosimagab/springboot_mongodb/repository/PostRepository.java
+++ src/main/java/com/motosimagab/springboot_mongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.motosimagab.springboot_mongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.motosimagab.springboot_mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	List<Post> findByTitleContainingIgnoringCase(String text); // query methods
 	
 }
